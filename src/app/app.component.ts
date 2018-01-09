6de16cc9a62c9a6e60f2aac6cabe280bf3bf0d46
Nikitha <a614343@fmr.com>
2018-01-09 14:10:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <p  [@myAwesomeAnimation]="state" (click)="animateMe()">I will animate</p>
+  `,
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      state('small', style({
+        transform: 'scale(1)'
+      })),
+      state('large', style({
+        transform: 'scale(1.2)'
+      })),
+      transition('small <=> large', animate('300ms ease-in', keyframes([
+        style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+        style({opacity: 1, transform: 'translateY(35px)', offset: .5}),
+        style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+      ])))
+
+    ]),
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  state = 'small';
+  titlestyles = {
+    'color': 'red',
+    'font-size': '4em'
+  };
+  animateMe() {
+    this.state = (this.state === 'small' ? 'large' : 'small');
+  }
 }
